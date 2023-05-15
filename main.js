c3d109b559224aee102cc674ab33ffa453f156b6
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-05-15 16:14:42 -0600

    Add exit button for mobile menu

--- main.js
+++ main.js
@@ -7,16 +7,25 @@ function displayBugerMenu(){
   } else {
   burgerMenu.style.display = 'none';
 }
-  
+}
 
+function exitBurguerMenu(){
+  console.log("I'm in the function exitBugerMenu")
+  const burgerMenu = document.getElementById('burgerMenu')
+  var menuDisplay = window.getComputedStyle(burgerMenu).display;
+  if(menuDisplay === 'block') {
+    burgerMenu.style.display = 'none';
+  }
 }
 
 console.log("Working");
 const burgerBtn = document.querySelector('#burgerBtn');
+const exitButton = document.getElementById('exitButton');
 console.log("burgerBtn");
 const burgerMenuAnchor = document.getElementById('burgerMenu').querySelectorAll('a');
 console.log("burgerMenu");
 
-burgerBtn.addEventListener('click', displayBugerMenu)
+burgerBtn.addEventListener('click', displayBugerMenu);
+exitButton.addEventListener('click', exitBurguerMenu);
 
   
