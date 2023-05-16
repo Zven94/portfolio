20bcadca50d44309812f5c69814fa1d87c91967e
Zven94 <125831572+Zven94@users.noreply.github.com>
2023-05-16 12:18:16 -0300

    Solved linters errors

--- main.js
+++ main.js
@@ -1,38 +1,37 @@
-function displayBugerMenu(){
-  const burgerMenu = document.getElementById('burgerMenu')
-  var menuDisplay = window.getComputedStyle(burgerMenu).display;
-  if(menuDisplay === 'none') {
+const burgerBtn = document.querySelector('#burgerBtn');
+const myLogo = document.querySelector('.my-logo');
+const exitButton = document.getElementById('exitButton');
+const burgerMenuAnchor = document.getElementById('burgerMenu').querySelectorAll('a');
+
+function displayBugerMenu() {
+  const burgerMenu = document.getElementById('burgerMenu');
+  const menuDisplay = window.getComputedStyle(burgerMenu).display;
+  if (menuDisplay === 'none') {
     burgerMenu.style.display = 'block';
     myLogo.style.display = 'none';
     burgerBtn.style.display = 'none';
   } else {
-  burgerMenu.style.display = 'none';
-  myLogo.style.display = 'block';
-  burgerBtn.style.display = 'block';
-}
-}
-
-function exitBurguerMenu(){
-  const burgerMenu = document.getElementById('burgerMenu')
-  var menuDisplay = window.getComputedStyle(burgerMenu).display;
-  if(menuDisplay === 'block') {
     burgerMenu.style.display = 'none';
     myLogo.style.display = 'block';
     burgerBtn.style.display = 'block';
   }
 }
 
-const burgerBtn = document.querySelector('#burgerBtn');
-const myLogo = document.querySelector(".my-logo")
-const exitButton = document.getElementById('exitButton');
-const burgerMenuAnchor = document.getElementById('burgerMenu').querySelectorAll('a');
+function exitBurguerMenu() {
+  const burgerMenu = document.getElementById('burgerMenu');
+  const menuDisplay = window.getComputedStyle(burgerMenu).display;
+  if (menuDisplay === 'block') {
+    burgerMenu.style.display = 'none';
+    myLogo.style.display = 'block';
+    burgerBtn.style.display = 'block';
+  }
+}
+
 burgerBtn.addEventListener('click', displayBugerMenu);
 exitButton.addEventListener('click', exitBurguerMenu);
 
-//add even listeners to each anchor witin burguer menu;
-for(let i = 0; i<burgerMenuAnchor.length; i++){
+// add even listeners to each anchor witin burguer menu;
+
+for (let i = 0; i < burgerMenuAnchor.length; i += 1) {
   burgerMenuAnchor[i].addEventListener('click', exitBurguerMenu);
 }
-
-
-  
