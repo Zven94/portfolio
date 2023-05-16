71a21b0a151a9a46edc1f3bf4ada1c724a1b0bb7
Zven94 <125831572+Zven94@users.noreply.github.com>
2023-05-16 00:01:40 -0300

    Added style to burgerMenu

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ function displayBugerMenu(){
   var menuDisplay = window.getComputedStyle(burgerMenu).display;
   if(menuDisplay === 'none') {
     burgerMenu.style.display = 'block';
+    myLogo.style.display = 'none';
+    burgerBtn.style.display = 'none';
   } else {
   burgerMenu.style.display = 'none';
+  myLogo.style.display = 'block';
+  burgerBtn.style.display = 'block';
 }
 }
 
@@ -13,10 +17,13 @@ function exitBurguerMenu(){
   var menuDisplay = window.getComputedStyle(burgerMenu).display;
   if(menuDisplay === 'block') {
     burgerMenu.style.display = 'none';
+    myLogo.style.display = 'block';
+    burgerBtn.style.display = 'block';
   }
 }
 
 const burgerBtn = document.querySelector('#burgerBtn');
+const myLogo = document.querySelector(".my-logo")
 const exitButton = document.getElementById('exitButton');
 const burgerMenuAnchor = document.getElementById('burgerMenu').querySelectorAll('a');
 burgerBtn.addEventListener('click', displayBugerMenu);
