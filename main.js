ad08141090cdf5cb389ab0862b7a406db48dedba
Zven94 <125831572+Zven94@users.noreply.github.com>
2023-05-17 22:11:37 -0300

    Solved linters errors

--- main.js
+++ main.js
@@ -8,6 +8,91 @@ const burgerMenuAnchor = document.getElementById('burgerMenu').querySelectorAll(
 const seeProjectsBtn = document.getElementById('seeProjectsBtn');
 const exitButtonPopup = document.getElementById('exitButton-popUp');
 
+// constructor function
+
+function MyCardTemplate(page, title, imgFile, h2, para, stack1, stack2, stack3, button) {
+  this.page = page;
+  this.title = title;
+  this.imgFile = imgFile;
+  this.h2 = h2;
+  this.para = para;
+  this.stack1 = stack1;
+  this.stack2 = stack2;
+  this.stack3 = stack3;
+  this.button = button;
+}
+
+// add project cards to html
+
+const myArr = [
+  new MyCardTemplate(
+    1,
+    'projectCards',
+    './images/project-example.png',
+    'Profesional Art Printing Data',
+    'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry´s standard.',
+    'HTML',
+    'Boostrap',
+    'Ruby',
+    'See project',
+  ),
+  new MyCardTemplate(
+    2,
+    'projectCards',
+    './images/project-example.png',
+    'Profesional Art Printing Data',
+    'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry´s standard.',
+    'HTML',
+    'Boostrap',
+    'Ruby',
+    'See project',
+  ),
+  new MyCardTemplate(
+    3,
+    'projectCards',
+    './images/project-example.png',
+    'Profesional Art Printing Data',
+    'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry´s standard.',
+    'HTML',
+    'Boostrap',
+    'Ruby',
+    'See project',
+  ),
+  new MyCardTemplate(
+    4,
+    'projectCards',
+    './images/project-example.png',
+    'Profesional Art Printing Data',
+    'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry´s standard.',
+    'HTML',
+    'Boostrap',
+    'Ruby',
+    'See project',
+  ),
+  new MyCardTemplate(
+    5,
+    'projectCards',
+    './images/project-example.png',
+    'Profesional Art Printing Data',
+    'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry´s standard.',
+    'HTML',
+    'Boostrap',
+    'Ruby',
+    'See project',
+  ),
+  new MyCardTemplate(
+    6,
+    'projectCards',
+    './images/project-example.png',
+    'Profesional Art Printing Data',
+    'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry´s standard.',
+    'HTML',
+    'Boostrap',
+    'Ruby',
+    'See project',
+  ),
+];
+
 /*  functions for buttons */
 
 function displayBugerMenu() {
@@ -34,115 +119,104 @@ function exitBurguerMenu() {
   }
 }
 
-function displayPopUp(){
+function displayPopUp() {
   const contPopUp = document.getElementById('contPopUp');
   const popUpDisplay = window.getComputedStyle(contPopUp).display;
   if (popUpDisplay === 'none') {
     contPopUp.style.display = 'block';
-/*    burgerBtn.style.display = 'none'; */
   } else {
     contPopUp.style.display = 'none';
-/*    burgerBtn.style.display = 'none'; */
   }
 }
 
-function exitPopUp () {
+function exitPopUp() {
   const contPopUp = document.getElementById('contPopUp');
   const popUpDisplay = window.getComputedStyle(contPopUp).display;
   if (popUpDisplay === 'block') {
     contPopUp.style.display = 'none';
-/*    burgerBtn.style.display = 'none'; */
+    /*    burgerBtn.style.display = 'none'; */
   } else {
     contPopUp.style.display = 'none';
-/*    burgerBtn.style.display = 'none'; */
+    /*    burgerBtn.style.display = 'none'; */
   }
 }
 
-function creatCardTemplate (i) {
-  
- //------- set parent where create cards -----
- const anchoPantalla = window.innerWidth;
- const contenedor = document.getElementById('projectCards');
- 
- //------- create div with a class "sec" -----
+function creatCardTemplate(i) {
+  // ------- set parent where create cards -----
+  const contenedor = document.getElementById('projectCards');
 
-  const divElement = document.createElement("div");
+  // ------- create div with a class "sec" -----
+
+  const divElement = document.createElement('div');
   divElement.classList.add('sec');
   contenedor.appendChild(divElement); // set the div inside the parent
 
-  //------- create img with a class "img-w1" -----
+  // ------- create img with a class "img-w1" -----
 
-  const imgElement = document.createElement("img");
+  const imgElement = document.createElement('img');
   divElement.appendChild(imgElement);
-  console.log(myArr[i]['imgFile']);
-  imgElement.src = myArr[i]['imgFile'];
-  imgElement.classList.add("img-w1");
+  imgElement.src = myArr[i].imgFile;
+  imgElement.classList.add('img-w1');
 
-  //------- create div 1 with a class "sec" -----
+  // ------- create div 1 with a class "sec" -----
 
-  const divElement1 = document.createElement("div");
+  const divElement1 = document.createElement('div');
   divElement1.classList.add('div-4');
   divElement.appendChild(divElement1); // set the div inside the div parent
 
-  //------- create div 2 with a class "sec" -----
+  // ------- create div 2 with a class "sec" -----
 
-  const divElement2 = document.createElement("div");
+  const divElement2 = document.createElement('div');
   divElement2.classList.add('sec');
-  divElement.appendChild(divElement2); // set the div inside the div parent  
+  divElement.appendChild(divElement2); // set the div inside the div parent
 
-  //------- create h2 with a class "header-4" -----
+  // ------- create h2 with a class "header-4" -----
 
-  const h2Element = document.createElement("h2");
+  const h2Element = document.createElement('h2');
   divElement1.appendChild(h2Element);
-  h2Element.textContent = myArr[i]['h2'];
-  h2Element.classList.add("header-4");
+  h2Element.textContent = myArr[i].h2;
+  h2Element.classList.add('header-4');
 
-  //------- create p with a class "para-3" -----
+  // ------- create p with a class "para-3" -----
 
-  const pElement = document.createElement("p");
+  const pElement = document.createElement('p');
   divElement1.appendChild(pElement);
-  pElement.textContent = myArr[i]['para'];
-  pElement.classList.add("para-3")
+  pElement.textContent = myArr[i].para;
+  pElement.classList.add('para-3');
 
-  //------- create ul with a class "ulDiv" & li to it -----
-  
-  //------- ul ------//
+  // ------- create ul with a class "ulDiv" & li to it -----
 
-  const ulElement = document.createElement("ul");
+  // ------- ul ------//
+
+  const ulElement = document.createElement('ul');
   divElement2.appendChild(ulElement);
-  ulElement.classList.add("tech-stack-2");
+  ulElement.classList.add('tech-stack-2');
 
-  //------- li ------//
+  // ------- li ------//
 
-  const liElement1 = document.createElement("li");
+  const liElement1 = document.createElement('li');
   ulElement.appendChild(liElement1);
-  liElement1.classList.add("h4-5");
-  liElement1.textContent = myArr[i]['stack1'];
+  liElement1.classList.add('h4-5');
+  liElement1.textContent = myArr[i].stack1;
 
-  const liElement2 = document.createElement("li");
+  const liElement2 = document.createElement('li');
   ulElement.appendChild(liElement2);
-  liElement2.classList.add("h4-6");
-  liElement2.textContent = myArr[i]['stack2'];
+  liElement2.classList.add('h4-6');
+  liElement2.textContent = myArr[i].stack2;
 
-  const liElement3 = document.createElement("li");
+  const liElement3 = document.createElement('li');
   ulElement.appendChild(liElement3);
-  liElement3.classList.add("h4-7");
-  liElement3.textContent = myArr[i]['stack3'];
+  liElement3.classList.add('h4-7');
+  liElement3.textContent = myArr[i].stack3;
 
-  //------- create button with a class "button-2" -----
+  // ------- create button with a class "button-2" -----
 
-  const btnElement = document.createElement("button");
+  const btnElement = document.createElement('button');
   divElement.appendChild(btnElement);
-  btnElement.classList.add("button-2");
-  btnElement.textContent = myArr[i]['button'];
-  btnElement.type = "sumit";
+  btnElement.classList.add('button-2');
+  btnElement.textContent = myArr[i].button;
+  btnElement.type = 'sumit';
   btnElement.addEventListener('click', displayPopUp);
-/*
-  return {
-    pageNumber:page;
-    pageTitle: 
-
-  }*/
 }
 
 // ----------------  Event Listener -----------------------//
@@ -152,108 +226,12 @@ exitButton.addEventListener('click', exitBurguerMenu);
 seeProjectsBtn.addEventListener('click', displayPopUp);
 exitButtonPopup.addEventListener('click', exitPopUp);
 
-
-
 // add even listeners to each anchor witin burguer menu;
 
 for (let i = 0; i < burgerMenuAnchor.length; i += 1) {
   burgerMenuAnchor[i].addEventListener('click', exitBurguerMenu);
 }
 
-// constructor function
-
-function myCardTemplate(page,title,imgFile,h2,para,stack1,stack2,stack3,button) {
-  this.page = page;
-  this.title = title;
-  this.imgFile = imgFile;
-  this.h2 = h2;
-  this.para = para;
-  this.stack1 = stack1;
-  this.stack2 = stack2;
-  this.stack3 = stack3;
-  this.button = button;
-}
-
-// add project cards to html
-/*
-let  myCard = {
-  page: 0,
-  title: "projectCards",
-  imgFile: "images/project-example.png",
-  h2: "Profesional Art Printing Data",
-  para: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-  stack1: "HTML",
-  stack2: "Boostrap",
-  stack3: "Ruby",
-  button: "See project",
-};*/
-
-
-let myArr = [ new myCardTemplate (
-  1,
-  "projectCards",
-  "./images/project-example.png",
-  "Profesional Art Printing Data",
-  "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-  "HTML",
-  "Boostrap",
-  "Ruby",
-  "See project"),
-  new myCardTemplate (
-  2,
-  "projectCards",
-  "./images/project-example.png",
-  "Profesional Art Printing Data",
-  "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-  "HTML",
-  "Boostrap",
-  "Ruby",
-  "See project"),
-  new myCardTemplate (
-  3,
-  "projectCards",
-  "./images/project-example.png",
-  "Profesional Art Printing Data",
-  "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-  "HTML",
-  "Boostrap",
-  "Ruby",
-  "See project"),
-  new myCardTemplate (
-  4,  
-  "projectCards",
-  "./images/project-example.png",
-  "Profesional Art Printing Data",
-  "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-  "HTML",
-  "Boostrap",
-  "Ruby",
-  "See project"),
-  new myCardTemplate (
-  5,  
-  "projectCards",
-  "./images/project-example.png",
-  "Profesional Art Printing Data",
-  "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-  "HTML",
-  "Boostrap",
-  "Ruby",
-  "See project"),
-  new myCardTemplate (
-  6,  
-  "projectCards",
-  "./images/project-example.png",
-  "Profesional Art Printing Data",
-  "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-  "HTML",
-  "Boostrap",
-  "Ruby",
-  "See project")
-];
-
-
 for (let i = 0; i < 6; i += 1) {
-
-  //call function to create card
-  creatCardTemplate(i)
-} 
+  creatCardTemplate(i); // call function to create card
+}
